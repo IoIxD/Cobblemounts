ffcc13acb5efc8961c93820e4309233636555a3c
IoIxD <alphaproject217@gmail.com>
2023-10-24 17:52:05 -0700

    version bump

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-./gradlew build 
-cp build/libs/cobblemounts-1.0.1.jar /home/gavin/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/1.20.1/.minecraft/mods/
+rm build/libs/cobblemounts-*.jar
+./gradlew build
+cp build/libs/cobblemounts-*.jar /home/gavin/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/1.20.1/.minecraft/mods/
