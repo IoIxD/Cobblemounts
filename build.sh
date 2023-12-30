58ba441dccccec050b21e4e195c56e400219fb1c
ipz233_vvv <jumpergooog@gmail.com>
2023-12-30 10:36:27 +0200

    remove all

--- build.sh
+++
@@ -1,3 +0,0 @@
-rm build/libs/cobblemounts-*.jar
-./gradlew build
-cp build/libs/cobblemounts-*.jar /home/gavin/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/1.20.1/.minecraft/mods/
